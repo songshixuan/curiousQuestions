1c1c116c4a060ac329b53433ee62215b27c3d9a5
ShixuanSong <bettercallssx@163.com>
2017-10-18 13:00:51 -0400

    thread:critical section

---
+++ Models/thread/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var glob int = 0
+var wg sync.WaitGroup
+var incMu sync.Mutex
+
+func tryunlock(i int) {
+	defer wg.Done()
+	fmt.Println("try Unlock", i)
+	incMu.Unlock()
+
+}
+
+func mutIncr(i int) {
+	defer wg.Done()
+	fmt.Println("increase", i)
+	loc := glob
+	for j := 0; j < i; j++ {
+		incMu.Lock()
+		loc = glob
+		loc++
+		glob = loc
+		incMu.Unlock()
+	}
+}
+
+func incr(i int) {
+	defer wg.Done()
+	fmt.Println("increase", i)
+	loc := glob
+	for j := 0; j < i; j++ {
+		//fmt.Println("execute")
+		loc = glob
+		loc++
+		glob = loc
+	}
+}
+
+func main() {
+	wg.Add(2)
+	go mutIncr(100000)
+	go mutIncr(100000)
+	wg.Wait()
+	fmt.Println("glob=", glob)
+}
